4287d4c62615783eb0391f9f6317e1f593b1b0e4
Arpit Gupta <51786177+arpit2205@users.noreply.github.com>
2020-04-17 15:52:38 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+    $(".hamburger-menu").click(function() {
+        $(".toggle").toggleClass("open")
+        $(".nav-list").toggleClass("open")
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1000
+    })
+})
+    
+
